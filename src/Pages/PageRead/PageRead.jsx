180684fc78f531f39bc3638cb9b38e1b2a8d9957
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-28 22:08:00 +0600

    pages and read bar chart validation and style finish

--- src/Pages/PageRead/PageRead.jsx
+++ src/Pages/PageRead/PageRead.jsx
@@ -1,7 +1,58 @@
+import React from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+import { getBookData } from "../../saveToBookStorage/saveToBookStorage";
+
 const PageRead = () => {
+  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+  const bookData = getBookData();
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
   return (
-    <div>
-      <h1>Page Read</h1>
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center mt-24">
+        <BarChart
+          width={800}
+          height={500}
+          data={bookData}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="bookName" />
+          <YAxis />
+          <Bar
+            dataKey="totalPages"
+            fill="#8884d8"
+            shape={<TriangleBar />}
+            label={{ position: "top" }}
+          >
+            {bookData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+            ))}
+          </Bar>
+        </BarChart>
+      </div>
     </div>
   );
 };
